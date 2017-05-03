d3b44f84e3a6fe0d282866dc57aa7c205ed74569
andre alves garzia <andre@andregarzia.com>
2017-05-03 16:21:58 -0300

    implementation(time picker):New time picker (flatpicker).

--- public/js/app.js
+++ public/js/app.js
@@ -247,3 +247,27 @@ for(var i = 0; i<allAddButtons.length; i++) {
 }
 
 
+/*
+ * Initialize pickers
+ */
+
+var timepickerConfiguration = {
+    enableTime: true,
+    noCalendar: true,
+
+    enableSeconds: false, // disabled by default
+
+    time_24hr: false, // AM/PM time picker is used by default
+
+    // default format
+    dateFormat: "H:i",
+
+    // initial values for time. don't use these to preload a date
+    defaultHour: 12,
+    defaultMinute: 0
+
+    // Preload time with defaultDate instead:
+    // defaultDate: "3:30"
+};
+
+$(".timepicker").flatpickr(timepickerConfiguration);
