c25cd7a8236f09b291dbacffe435034860dc3a20
Whiteroom <akasha@WhiteroomMacbook.local>
2018-06-13 17:07:00 -1000

    this is pk's version

--- public/js/app.js
+++ public/js/app.js
@@ -251,24 +251,12 @@ for(var i = 0; i<allAddButtons.length; i++) {
  * Initialize pickers
  */
 
-var timepickerConfiguration = {
-    enableTime: true,
-    noCalendar: true,
+ var timepickerConfiguration = {
+     step: 30,
+     disableTextInput: true,
+     'minTime': '6:00am',
+    'maxTime': '6:00pm',
+ };
 
-    enableSeconds: false, // disabled by default
-
-    time_24hr: false, // AM/PM time picker is used by default
-
-    // default format
-    dateFormat: "h:i K",
-
-    // initial values for time. don't use these to preload a date
-    defaultHour: 12,
-    defaultMinute: 0
-
-    // Preload time with defaultDate instead:
-    // defaultDate: "3:30"
-};
-
-$(".timepicker").flatpickr(timepickerConfiguration);
+$(".timepicker").timepicker(timepickerConfiguration);
 $(".datepicker").flatpickr();
---
+++ run.sh
@@ -0,0 +1,1 @@
+php -S 192.168.108.186:8888 -t public public/index.php
